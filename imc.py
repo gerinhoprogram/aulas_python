a3e5c6e11d78ef9874fe4c966a21d30993ad5775
gerinhoprogram <rogerio_furquim@hotmail.com>
2023-11-30 22:00:01 -0300

    imc

---
+++ imc.py
@@ -0,0 +1,11 @@
+nome = 'Rogério'
+altura = 1.75
+peso = 59
+imc = peso / altura ** 2
+
+linha_1 = f'{nome} tem {altura:.2f} de altura'
+
+print(linha_1)
+print('pesa', peso, 'quilos e seu imc é',)
+print(imc)
+
