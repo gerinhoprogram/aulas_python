a416bfcb2cc6cde20a7b3b5f7dd4b1e04820dcc2
gerinhoprogram <rogerio_furquim@hotmail.com>
2023-12-01 22:32:05 -0300

    comparacao2

---
+++ comparacao_2.py
@@ -0,0 +1,12 @@
+primeiro = input('Number 1: ')
+segundo = input('number 2: ')
+
+if primeiro >= segundo:
+    print(
+        f'{primeiro=} é maior ou igual'
+    )
+else:
+    print(
+        f'{segundo=} é maior'
+        f'do que {primeiro=}'
+    )
